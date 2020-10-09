630c712425937bec742ad1a46b7ba0fd953e1fdf
Suan Tonsing <suanasdf@gmail.com>
2020-10-09 21:03:01 +0530

    Update Homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,74 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ScrollView, View, Text, TouchableOpacity} from 'react-native';
+import axios from 'axios';
 
-export default function HomeScreen() {
+//components
+import Spinner from '../components/Spinner';
+import Recommendations from '../components/Recommendations';
+
+export default function HomeScreen({navigation}) {
+  const [recommendations, setRecommendations] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function getRecommendations() {
+    return axios.get(
+      'https://joeswantonsing.github.io/Laibu_DATA/api/recommended.json',
+    );
+  }
+
+  function getTrending() {
+    return axios.get(
+      'https://joeswantonsing.github.io/Laibu_DATA/api/trending.json',
+    );
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    Promise.all([getRecommendations(), getTrending()])
+      .then(function (results) {
+        setRecommendations(results[0].data);
+        setTrending(results[1].data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function renderScreen() {
+    return (
+      <View>
+        <Recommendations
+          navigation={navigation}
+          sectionTitle="Recommended Books for you"
+          items={recommendations}
+        />
+        <Recommendations
+          navigation={navigation}
+          sectionTitle="Trending Books"
+          items={trending}
+        />
+      </View>
+    );
+  }
   return (
-    <View>
-      <Text>Hello HomeScreen</Text>
-    </View>
+    <ScrollView style={styles.scrollViewStyle}>
+      {isLoading ? (
+        <View style={styles.spinnerContainer}>
+          <Spinner size="large" color="#EA8953" spinnerText="Loading" />
+        </View>
+      ) : (
+        renderScreen()
+      )}
+    </ScrollView>
   );
 }
+
+const styles = {
+  scrollViewStyle: {
+    padding: 7,
+    paddingTop: 10,
+    backgroundColor: '#fff',
+  },
+};

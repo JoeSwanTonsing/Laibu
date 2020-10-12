6d719a9cf060278801fe2cf41b455fb7959d2626
Suan Tonsing <suanasdf@gmail.com>
2020-10-12 22:55:10 +0530

    Remake

--- src/screens/ItemDetailsScreen.js
+++ src/screens/ItemDetailsScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   Image,
@@ -7,41 +7,120 @@ import {
   ScrollView,
   TouchableOpacity,
 } from 'react-native';
+import axios from 'axios';
+
+//my components
+import Spinner from '../components/Spinner';
 
 export default function ItemDetailsScreen({navigation, route}) {
-  const {item} = route.params;
+  const {item_id} = route.params;
+  const [details, setDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const {id, title, author, authorImage, price, rating, year, image} = item;
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get('http://192.168.1.8/laibu/api/books/', {
+        params: {
+          book_id: item_id,
+        },
+      })
+      .then((res) => {
+        setDetails(res.data[0]);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const {
+    title,
+    subtitle,
+    description,
+    price,
+    rating,
+    year,
+    thumbnail,
+    pages,
+    author_name,
+    genre_name,
+  } = details;
+
+  function getSubtitle() {
+    if (subtitle !== '' && subtitle != null) {
+      return <Text style={styles.subTitleText}>{subtitle}</Text>;
+    }
+    return null;
+  }
+
+  function getYear() {
+    if (year !== '' && year != null) {
+      return <Text style={styles.yearText}>{year}</Text>;
+    }
+    return null;
+  }
+
+  function getDescription() {
+    if (description !== '' && description != null) {
+      return <Text style={styles.descriptionText}>{description}</Text>;
+    }
+    return <Text style={styles.descriptionText}>Description Unavailable</Text>;
+  }
+
+  function getPrice() {
+    if (price === '0') {
+      return (
+        <TouchableOpacity style={styles.buyBTN}>
+          <Text style={styles.buyBTNText}>Get It For Free!</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      return (
+        <TouchableOpacity style={styles.buyBTN}>
+          <Text style={styles.buyBTNText}>Buy Now</Text>
+          <Text style={styles.buyBTNText}>
+            {'\u20B9'}
+            {price}
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+  }
+
+  function renderDetails() {
+    return (
+      <View style={styles.content}>
+        <View style={styles.introContent}>
+          <View style={styles.thumbnailContainer}>
+            <Image style={styles.thumbnail} source={{uri: thumbnail}} />
+          </View>
+          <View style={styles.descriptionContainer}>
+            <Text style={styles.titleText}>{title}</Text>
+            {getSubtitle()}
+            <Text style={styles.authorText}>{author_name}</Text>
+            {getYear()}
+          </View>
+        </View>
+        {getPrice()}
+        <View style={styles.aboutSection}>
+          <Text style={styles.titleText}>Description:</Text>
+          {getDescription()}
+        </View>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View style={styles.content}>
-          <View style={styles.introContent}>
-            <View style={styles.thumbnailContainer}>
-              <Image style={styles.thumbnail} source={{uri: image}} />
-            </View>
-            <View style={styles.descriptionContainer}>
-              <Text style={styles.titleText} numberOfLines={1}>
-                {title}
-              </Text>
-              <Text style={styles.authorText} numberOfLines={1}>
-                {author}
-              </Text>
-              <Text style={styles.yearText}>{year}</Text>
-            </View>
+        {isLoading ? (
+          <View style={styles.spinnerContainer}>
+            <Spinner size="large" color="#EA8953" spinnerText="Loading" />
           </View>
-          <TouchableOpacity style={styles.buyBTN}>
-            <Text style={styles.buyBTNText}>Buy Now</Text>
-            <Text style={styles.buyBTNText}>
-              {'\u20B9'}
-              {price}
-            </Text>
-          </TouchableOpacity>
-          <View style={styles.aboutSection}>
-            <Text style={styles.titleText}>Description:</Text>
-            <Text style={styles.descriptionText}> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
-          </View>
-        </View>
+        ) : (
+          renderDetails()
+        )}
       </ScrollView>
     </SafeAreaView>
   );
@@ -85,6 +164,11 @@ const styles = {
     fontWeight: 'bold',
     color: '#293241',
     fontSize: 16,
+  },
+  subTitleText: {
+    color: 'grey',
+    fontSize: 15,
+    fontWeight: 'bold',
   },
   authorText: {
     color: 'grey',

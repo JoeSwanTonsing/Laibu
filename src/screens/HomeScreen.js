68ca9922431dcb5496a21f794eebe73088147f38
Suan Tonsing <suanasdf@gmail.com>
2020-10-16 00:03:19 +0530

    changed api path

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,11 +18,11 @@ export default function HomeScreen({navigation}) {
   const [isLoading, setIsLoading] = useState(false);
 
   function getRecommendations() {
-    return axios.get('http://192.168.1.9/laibu/api/allBookListing/');
+    return axios.get('http://192.168.1.9:80/laibu-api/api/allBookListing/');
   }
 
   function getTrending() {
-    return axios.get('http://192.168.1.9/laibu/api/allBookListing/');
+    return axios.get('http://192.168.1.9:80/laibu-api/api/allBookListing/');
   }
 
   useEffect(() => {

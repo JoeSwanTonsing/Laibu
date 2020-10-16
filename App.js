801b011e17d8f60fb5d37c848bd82235048d4a6f
Suan Tonsing <suanasdf@gmail.com>
2020-10-16 13:02:37 +0530

    Added statusbar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {ScrollView, View, Text} from 'react-native';
-import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {StatusBar} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -14,30 +14,33 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
+    <SafeAreaProvider>
+      <StatusBar barStyle="light-content" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused ? 'home-sharp' : 'home-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings-sharp' : 'settings-outline';
-            } else if (route.name === 'MyBooks') {
-              iconName = focused ? 'book-sharp' : 'book-outline';
-            }
-            return <Icon name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: '#EA8953',
-          inactiveTintColor: 'grey',
-        }}>
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="MyBooks" component={MyBooksStack} />
-        <Tab.Screen name="Settings" component={SettingsStack} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              if (route.name === 'Home') {
+                iconName = focused ? 'home-sharp' : 'home-outline';
+              } else if (route.name === 'Settings') {
+                iconName = focused ? 'settings-sharp' : 'settings-outline';
+              } else if (route.name === 'MyBooks') {
+                iconName = focused ? 'book-sharp' : 'book-outline';
+              }
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: '#EA8953',
+            inactiveTintColor: 'grey',
+          }}>
+          <Tab.Screen name="Home" component={HomeStack} />
+          <Tab.Screen name="MyBooks" component={MyBooksStack} />
+          <Tab.Screen name="Settings" component={SettingsStack} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
